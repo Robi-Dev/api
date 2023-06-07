e0da21258a6e4541faf8a55d2a76263b949755ac
Zelal Hossain <robi@ojjomedia.com>
2023-06-07 13:05:00 +0600

    Update endpoint

--- src/api/themes/public/index.ts
+++ src/api/themes/public/index.ts
@@ -4,6 +4,6 @@ import { getAllPublicThemesHandler } from '../../../controller/theme.controller'
 const router = express.Router();
 
 // get all theme public
-router.get('/:type', getAllPublicThemesHandler);
+router.get('/:type', (req, res) => getAllPublicThemesHandler(req, res));
 
 export default router;
--- src/controller/theme.controller.ts
+++ src/controller/theme.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { updateStatus } from '../service/section.service';
 import {
   createTheme,
   getThemeById,
